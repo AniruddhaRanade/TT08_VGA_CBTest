6e6d4d8116010d2de9ee5fc7d7c07eaa0bf3e3a4
Aniruddha Ranade <18034541+AniruddhaRanade@users.noreply.github.com>
2024-09-02 21:31:48 +0530

    Update

--- src/project.v
+++ src/project.v
@@ -48,8 +48,14 @@ module tt_um_example (
     .vpos(pix_y)
   );
   
-  //wire [9:0] moving_x = pix_x + counter;
 
+    wire [9:0] moving_x = pix_x + counter;
+
+    assign R = video_active ? {moving_x[5], pix_y[2]} : 2'b00;
+    assign G = video_active ? {moving_x[6], pix_y[2]} : 2'b00;
+    assign B = video_active ? {moving_x[7], pix_y[5]} : 2'b00;
+    
+    /*	
     parameter [9:0] CIRCLE_CENTER_X = 320; 
     parameter [9:0] CIRCLE_CENTER_Y = 240; 
     parameter [9:0] CIRCLE_RADIUS = 200;   
@@ -84,8 +90,9 @@ module tt_um_example (
     assign B = (video_active && in_circle) ? {moving_y[3] & combined[6], moving_x[4] ~^ pix_y[0]} : 2'b00;
     assign B = (video_active && in_circle) ? {combined[5] ^ pix_y[7], moving_x[1] & moving_y[2]} : 2'b00;
 
+  */
 
-  /*
+
   always @(posedge vsync) begin
     if (~rst_n) begin
       counter <= 0;
@@ -93,5 +100,70 @@ module tt_um_example (
       counter <= counter + 1;
     end
   end
+  
+endmodule
+
+/*
+Video sync generator, used to drive a VGA monitor.
+Timing from: https://en.wikipedia.org/wiki/Video_Graphics_Array
+To use:
+- Wire the hsync and vsync signals to top level outputs
+- Add a 3-bit (or more) "rgb" output to the top level
 */
+
+module hvsync_generator(clk, reset, hsync, vsync, display_on, hpos, vpos);
+
+  input clk;
+  input reset;
+  output reg hsync, vsync;
+  output display_on;
+  output reg [9:0] hpos;
+  output reg [9:0] vpos;
+
+  // declarations for TV-simulator sync parameters
+  // horizontal constants
+  parameter H_DISPLAY       = 640; // horizontal display width
+  parameter H_BACK          =  48; // horizontal left border (back porch)
+  parameter H_FRONT         =  16; // horizontal right border (front porch)
+  parameter H_SYNC          =  96; // horizontal sync width
+  // vertical constants
+  parameter V_DISPLAY       = 480; // vertical display height
+  parameter V_TOP           =  33; // vertical top border
+  parameter V_BOTTOM        =  10; // vertical bottom border
+  parameter V_SYNC          =   2; // vertical sync # lines
+  // derived constants
+  parameter H_SYNC_START    = H_DISPLAY + H_FRONT;
+  parameter H_SYNC_END      = H_DISPLAY + H_FRONT + H_SYNC - 1;
+  parameter H_MAX           = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1;
+  parameter V_SYNC_START    = V_DISPLAY + V_BOTTOM;
+  parameter V_SYNC_END      = V_DISPLAY + V_BOTTOM + V_SYNC - 1;
+  parameter V_MAX           = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1;
+
+  wire hmaxxed = (hpos == H_MAX) || reset;	// set when hpos is maximum
+  wire vmaxxed = (vpos == V_MAX) || reset;	// set when vpos is maximum
+  
+  // horizontal position counter
+  always @(posedge clk)
+  begin
+    hsync <= (hpos>=H_SYNC_START && hpos<=H_SYNC_END);
+    if(hmaxxed)
+      hpos <= 0;
+    else
+      hpos <= hpos + 1;
+  end
+
+  // vertical position counter
+  always @(posedge clk)
+  begin
+    vsync <= (vpos>=V_SYNC_START && vpos<=V_SYNC_END);
+    if(hmaxxed)
+      if (vmaxxed)
+        vpos <= 0;
+      else
+        vpos <= vpos + 1;
+  end
+  
+  // display_on is set when beam is in "safe" visible frame
+  assign display_on = (hpos<H_DISPLAY) && (vpos<V_DISPLAY);
+
 endmodule
