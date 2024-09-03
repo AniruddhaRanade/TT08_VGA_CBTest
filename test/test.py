babadcfc68095c7822f36a7e6f0d42d113ada28e
Aniruddha Ranade <18034541+AniruddhaRanade@users.noreply.github.com>
2024-09-03 23:48:37 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -16,9 +16,9 @@ async def test_project(dut):
 
     # Reset
     dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
+    # dut.ena.value = 1
+    # dut.ui_in.value = 0
+    # dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
@@ -26,8 +26,8 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # dut.ui_in.value = 20
+    # dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
