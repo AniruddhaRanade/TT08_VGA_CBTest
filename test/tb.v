18cf403b8823fd249ebf57224656d6176f2f41e4
Aniruddha Ranade <18034541+AniruddhaRanade@users.noreply.github.com>
2024-09-03 23:44:35 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -32,12 +32,12 @@ module tb ();
       .VGND(1'b0),
 `endif
 
-      .ui_in  (ui_in),    // Dedicated inputs
+      // .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+      // .uio_in (uio_in),   // IOs: Input path
+      // .uio_out(uio_out),  // IOs: Output path
+      // .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      // .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
