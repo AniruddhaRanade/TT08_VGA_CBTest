7224002f63ff9eec4092ef27f8dedd192ba7cece
Krushnasis Pradhan <172490314+Krushnasis-Pradhan@users.noreply.github.com>
2024-09-05 12:17:28 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,140 @@
 
 `default_nettype none
 
-module bitmap_rom (
+parameter LOGO_SIZE = 128;  // Size of the logo in pixels
+parameter DISPLAY_WIDTH = 640;  // VGA display width
+parameter DISPLAY_HEIGHT = 480;  // VGA display height
+
+`define COLOR_WHITE 3'd7
+
+module tt_um_tinytapeout_logo_screensaver (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+  // VGA signals
+  wire hsync;
+  wire vsync;
+  reg [1:0] R;
+  reg [1:0] G;
+  reg [1:0] B;
+  wire video_active;
+  wire [9:0] pix_x;
+  wire [9:0] pix_y;
+
+  // Configuration
+  wire cfg_tile = ui_in[0];
+  wire cfg_solid_color = ui_in[1];
+
+  // TinyVGA PMOD
+  assign uo_out  = {hsync, B[0], G[0], R[0], vsync, B[1], G[1], R[1]};
+
+  // Unused outputs assigned to 0.
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // Suppress unused signals warning
+  wire _unused_ok = &{ena, ui_in[7:1], uio_in};
+
+  reg [9:0] prev_y;
+
+  vga_sync_generator vga_sync_gen (
+      .clk(clk),
+      .reset(~rst_n),
+      .hsync(hsync),
+      .vsync(vsync),
+      .display_on(video_active),
+      .hpos(pix_x),
+      .vpos(pix_y)
+  );
+
+  reg [9:0] logo_left;
+  reg [9:0] logo_top;
+  reg dir_x;
+  reg dir_y;
+
+  wire pixel_value;
+  reg [2:0] color_index;
+  wire [5:0] pallete_color;
+  wire [5:0] color;
+
+  wire [9:0] x = pix_x - logo_left;
+  wire [9:0] y = pix_y - logo_top;
+  wire logo_pixels = cfg_tile || (x[9:7] == 0 && y[9:7] == 0);
+
+  bitmap_rom rom1 (
+      .x(x[6:0]),
+      .y(y[6:0]),
+      .pixel(pixel_value)
+  );
+
+  palette palette_inst (
+      .color_index(color_index),
+      .rrggbb(pallete_color)
+  );
+
+  wire [5:0] gradient_color = {1'b1, y[6:2] - x[6:2] + logo_left[6:2]};
+  assign color = cfg_solid_color ? pallete_color : gradient_color;
+
+  // RGB output logic
+  always @(posedge clk) begin
+    if (~rst_n) begin
+      R <= 0;
+      G <= 0;
+      B <= 0;
+    end else begin
+      R <= 0;
+      G <= 0;
+      B <= 0;
+      if (video_active && logo_pixels) begin
+        R <= pixel_value ? color[5:4] : 0;
+        G <= pixel_value ? color[3:2] : 0;
+        B <= pixel_value ? color[1:0] : 0;
+      end
+    end
+  end
+
+  // Bouncing logic
+  always @(posedge clk) begin
+    if (~rst_n) begin
+      logo_left <= 200;
+      logo_top <= 200;
+      dir_y <= 0;
+      dir_x <= 1;
+      color_index <= 0;
+    end else begin
+      prev_y <= pix_y;
+      if (pix_y == 0 && prev_y != pix_y) begin
+        logo_left <= logo_left + (dir_x ? 1 : -1);
+        logo_top  <= logo_top + (dir_y ? 1 : -1);
+        if (logo_left - 1 == 0 && !dir_x) begin
+          dir_x <= 1;
+          color_index <= color_index + 1;
+        end
+        if (logo_left + 1 == DISPLAY_WIDTH - LOGO_SIZE && dir_x) begin
+          dir_x <= 0;
+          color_index <= color_index + 1;
+        end
+        if (logo_top - 1 == 0 && !dir_y) begin
+          dir_y <= 1;
+          color_index <= color_index + 1;
+        end
+        if (logo_top + 1 == DISPLAY_HEIGHT - LOGO_SIZE && dir_y) begin
+          dir_y <= 0;
+          color_index <= color_index + 1;
+        end
+      end
+    end
+  end
+
+endmodule
+module tt_um_vga_example (
     input wire [6:0] x,
     input wire [6:0] y,
     output wire pixel
@@ -4119,6 +4252,29 @@ end
 endmodule
 
 
+module palette (
+    input  wire [2:0] color_index,
+    output wire [5:0] rrggbb
+);
+
+  reg [5:0] palette[7:0];
+
+  initial begin
+    palette[0] = 6'b001011;  // cyan
+    palette[1] = 6'b110110;  // pink
+    palette[2] = 6'b101101;  // green
+    palette[3] = 6'b111000;  // orange
+    palette[4] = 6'b110011;  // purple
+    palette[5] = 6'b111100;  // yellow
+    palette[6] = 6'b110001;  // red
+    palette[7] = 6'b111111;  // white
+  end
+
+  assign rrggbb = palette[color_index];
+
+endmodule
+
+
 // module tt_um_vga_example(
 //   input  wire [7:0] ui_in,    // Dedicated inputs
 //   output wire [7:0] uo_out,   // Dedicated outputs
@@ -4237,7 +4393,15 @@ To use:
 - Add a 3-bit (or more) "rgb" output to the top level
 */
 
-module hvsync_generator(clk, reset, hsync, vsync, display_on, hpos, vpos);
+module vga_sync_generator (
+    clk,
+    reset,
+    hsync,
+    vsync,
+    display_on,
+    hpos,
+    vpos
+);
 
   input clk;
   input reset;
@@ -4248,48 +4412,42 @@ module hvsync_generator(clk, reset, hsync, vsync, display_on, hpos, vpos);
 
   // declarations for TV-simulator sync parameters
   // horizontal constants
-  parameter H_DISPLAY       = 640; // horizontal display width
-  parameter H_BACK          =  48; // horizontal left border (back porch)
-  parameter H_FRONT         =  16; // horizontal right border (front porch)
-  parameter H_SYNC          =  96; // horizontal sync width
+  parameter H_DISPLAY = 640;  // horizontal display width
+  parameter H_BACK = 48;  // horizontal left border (back porch)
+  parameter H_FRONT = 16;  // horizontal right border (front porch)
+  parameter H_SYNC = 96;  // horizontal sync width
   // vertical constants
-  parameter V_DISPLAY       = 480; // vertical display height
-  parameter V_TOP           =  33; // vertical top border
-  parameter V_BOTTOM        =  10; // vertical bottom border
-  parameter V_SYNC          =   2; // vertical sync # lines
+  parameter V_DISPLAY = 480;  // vertical display height
+  parameter V_TOP = 33;  // vertical top border
+  parameter V_BOTTOM = 10;  // vertical bottom border
+  parameter V_SYNC = 2;  // vertical sync # lines
   // derived constants
-  parameter H_SYNC_START    = H_DISPLAY + H_FRONT;
-  parameter H_SYNC_END      = H_DISPLAY + H_FRONT + H_SYNC - 1;
-  parameter H_MAX           = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1;
-  parameter V_SYNC_START    = V_DISPLAY + V_BOTTOM;
-  parameter V_SYNC_END      = V_DISPLAY + V_BOTTOM + V_SYNC - 1;
-  parameter V_MAX           = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1;
+  parameter H_SYNC_START = H_DISPLAY + H_FRONT;
+  parameter H_SYNC_END = H_DISPLAY + H_FRONT + H_SYNC - 1;
+  parameter H_MAX = H_DISPLAY + H_BACK + H_FRONT + H_SYNC - 1;
+  parameter V_SYNC_START = V_DISPLAY + V_BOTTOM;
+  parameter V_SYNC_END = V_DISPLAY + V_BOTTOM + V_SYNC - 1;
+  parameter V_MAX = V_DISPLAY + V_TOP + V_BOTTOM + V_SYNC - 1;
 
-  wire hmaxxed = (hpos == H_MAX) || reset;	// set when hpos is maximum
-  wire vmaxxed = (vpos == V_MAX) || reset;	// set when vpos is maximum
-  
+  wire hmaxxed = (hpos == H_MAX) || reset;  // set when hpos is maximum
+  wire vmaxxed = (vpos == V_MAX) || reset;  // set when vpos is maximum
+
   // horizontal position counter
-  always @(posedge clk)
-  begin
-    hsync <= (hpos>=H_SYNC_START && hpos<=H_SYNC_END);
-    if(hmaxxed)
-      hpos <= 0;
-    else
-      hpos <= hpos + 1;
+  always @(posedge clk) begin
+    hsync <= (hpos >= H_SYNC_START && hpos <= H_SYNC_END);
+    if (hmaxxed) hpos <= 0;
+    else hpos <= hpos + 1;
   end
 
   // vertical position counter
-  always @(posedge clk)
-  begin
-    vsync <= (vpos>=V_SYNC_START && vpos<=V_SYNC_END);
-    if(hmaxxed)
-      if (vmaxxed)
-        vpos <= 0;
-      else
-        vpos <= vpos + 1;
+  always @(posedge clk) begin
+    vsync <= (vpos >= V_SYNC_START && vpos <= V_SYNC_END);
+    if (hmaxxed)
+      if (vmaxxed) vpos <= 0;
+      else vpos <= vpos + 1;
   end
-  
+
   // display_on is set when beam is in "safe" visible frame
-  assign display_on = (hpos<H_DISPLAY) && (vpos<V_DISPLAY);
+  assign display_on = (hpos < H_DISPLAY) && (vpos < V_DISPLAY);
 
 endmodule
