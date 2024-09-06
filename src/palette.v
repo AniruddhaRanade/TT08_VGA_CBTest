196ae309abd8e6807e9fb463657b9a9f5e5bcce8
Aniruddha Ranade <18034541+AniruddhaRanade@users.noreply.github.com>
2024-09-06 23:06:05 +0530

    Update palette.v

--- src/palette.v
+++ src/palette.v
@@ -7,14 +7,14 @@ module palette (
   reg [5:0] palette[7:0];
 
   initial begin
-    palette[0] = 6'b001011;  // cyan
-    palette[1] = 6'b110110;  // pink
-    palette[2] = 6'b101101;  // green
-    palette[3] = 6'b111000;  // orange
-    palette[4] = 6'b110011;  // purple
-    palette[5] = 6'b111100;  // yellow
-    palette[6] = 6'b110001;  // red
-    palette[7] = 6'b111111;  // white
+    palette[0] = 6'b111000;
+    palette[1] = 6'b110110;  
+    palette[2] = 6'b101101; 
+    palette[3] = 6'b111000;  
+    palette[4] = 6'b110011;  
+    palette[5] = 6'b011111;   
+    palette[6] = 6'b001011;  
+    palette[7] = 6'b000000;  // black
   end
 
   assign rrggbb = palette[color_index];
